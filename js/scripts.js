16e8d92f4befdd394d07babfeae493d7e711e6de
Andrew Glines and Olena Kuchko <andrew+olena@>
2017-05-15 10:34:15 -0700

    add initialBag, generate rack

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // list of objects
 
+// put bag array in here until JSON works
+var initialBag = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E"];
 // player
 
 // bag
@@ -9,26 +11,44 @@
 
 function Bag() {
   this.bagTiles = [];
-}
+};
 
 function Player(name, score, rack) {
   this.name = name;
   this.score = score;
   this.rack = rack;
-}
+};
 
 function Cell(x, y, pointMultiplier) {
   this.x = x;
   this.y = y;
   this.pointMultiplier = pointMultiplier;
-}
+};
 
 function Rack() {
   this.rackTiles = [];
-}
-Rack.prototype.generateRack(){
+  this.needNumber = 7;
+};
 
+Rack.prototype.generateRack = function (needNumber,initialBag) {
+  for (var i = 0; i < needNumber; i++) {
+    var currentRandomInt = getRandomInt(0, 20);
+    this.rackTiles.push(initialBag[currentRandomInt]);
+    initialBag.splice(currentRandomInt, 1);
+  };
+  // random number between 1 and length of array
+  // splice that number, place in Player's rack
+  // repeat until player rack full
 }
+
+
+
+
+function getRandomInt(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min)) + min;
+};
 
 
 // cell on board
@@ -38,3 +58,13 @@ Rack.prototype.generateRack(){
 // rack
 
 //
+$(function () {
+  // var word = JSON.parse(bag);
+  // // console.log(JSON.parse(words));
+  // console.log(JSON.parse(word));
+  // console.log(word[1]);
+  var rack = new Rack();
+  rack.generateRack(7, initialBag);
+  console.log(rack);
+  console.log(initialBag);
+});
