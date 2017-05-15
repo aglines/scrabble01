ae810c4e44f55cb795a693de7ae6d1a071bb76c9
Andrew Glines and Olena Kuchko <andrew+olena@>
2017-05-15 15:56:30 -0700

    add code to check if a word is a valid word, to score a word, and to treat tiles as objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 
-var initialBag = ["A", "A", "B", "C", "F", "P", "O", "O", "T", "R", "S","C","V","D","I","I","N","N","M","M","E","R","R","T","L","L","Q","Z","H","J","G","_"];
+var testLetters = [ "A", "B", "C", "F", "P", "O", "O", "T", "R", "S","C","V","D","I","I","N","N","M","M","E","R","R","T","L","L","Q","Z","H","J","G","_"];
+var initialBag = [];
+for (var i = 0; i < 15; i++) {
+  // this is testing code;  later, check actual letterValues
+  var tile = new Tile(testLetters[i], i);
+  initialBag.push(tile);
+}
+// console.log(arrayObj);
+
+// test dictionary
+var dictionary = ["cat", "tree", "rain", "wind"];
 
 function Bag() {
   this.bagTiles = [];
 };
 
-function Board() {
+function Game() {
 };
-
 function Player(name, rack) {
   this.name = name;
   this.score = 0;
@@ -15,11 +24,15 @@ function Player(name, rack) {
   this.currentWord = [];
 };
 
-function Cell(x, y, pointMultiplier, value) {
+function Cell(x, y, pointMultiplier) {
   this.x = x;
   this.y = y;
   this.pointMultiplier = pointMultiplier;
-  this.value = value;
+  this.tile = {};
+};
+function Tile(letter, letterValue) {
+  this.letter = letter;
+  this.letterValue = letterValue;
 };
 
 function Rack() {
@@ -31,7 +44,7 @@ Player.prototype.buildWord = function (tile) {
   return this.currentWord.push(tile);
 };
 
-Board.prototype.checkHorizontalPosition = function (currentWord) {
+Game.prototype.checkHorizontalPosition = function (currentWord) {
   var checkHorizontal;
   for (var i = 0; i < currentWord.length-1; i++) {
     console.log("currentWord[i] = ", currentWord[i]);
@@ -45,7 +58,7 @@ Board.prototype.checkHorizontalPosition = function (currentWord) {
   }
   return checkHorizontal;
 };
-Board.prototype.checkVerticalPosition = function (currentWord) {
+Game.prototype.checkVerticalPosition = function (currentWord) {
   var checkVertical;
   for (var i = 0; i < currentWord.length-1; i++) {
     if (currentWord[i].y === currentWord[i+1].y) {
@@ -57,9 +70,34 @@ Board.prototype.checkVerticalPosition = function (currentWord) {
   return checkVertical;
 };
 
+Game.prototype.countScore = function(currentWord, cells) {
+  var currentWordScore = 0;
+  debugger;
+  for (var i = 0; i < currentWord.length; i++) {
+    // count any letters with double letter score;
+    if ( cells[i].pointMultiplier === parseInt("2") ) {
+      currentWord[i].letterValue *= 2;
+    }
+    // add all individual tile counts (with above);
+    currentWordScore += currentWord[i].letterValue;
+
+    // multiply any word-level multipliers (2w, 3W);
+    if (cells[i].pointMultiplier === "2W") {
+      currentWordScore *= 2;
+      }
+  }
+  return currentWordScore;
+  console.log("current word score = ", currentWordScore);
+};
+
+Game.prototype.checkValidWord = function (currentWord) {
+ return dictionary.includes(currentWord);
+
+};
+
 Rack.prototype.generateRack = function (needNumber,initialBag) {
   for (var i = 0; i < needNumber; i++) {
-    var currentRandomInt = getRandomInt(0, 20);
+    var currentRandomInt = getRandomInt(0, 15);
     this.rackTiles.push(initialBag[currentRandomInt]);
     initialBag.splice(currentRandomInt, 1);
   };
@@ -79,7 +117,7 @@ $(function () {
   var rack = new Rack();
   rack.generateRack(7, initialBag);
 
-  var scrabbleBoard = new Board();
+  var scrabbleGame = new Game();
   var player = new Player ("Tom", rack);
   // console.log("player = ", player);
   var cells=[];
@@ -87,29 +125,28 @@ $(function () {
     var cell = new Cell(i, j, 2, rack.rackTiles[i]);
     cells.push(cell);
     player.buildWord(rack.rackTiles[i]);
-    console.log("word = ", player.currentWord);
+    // console.log("word = ", player.currentWord);
     // console.log("rack.rackTiles[i] = ",rack.rackTiles[i]);
   }
-  scrabbleBoard.checkHorizontalPosition(cells);
-  scrabbleBoard.checkVerticalPosition(cells);
-  console.log(scrabbleBoard.checkHorizontalPosition(cells));
-  console.log(scrabbleBoard.checkVerticalPosition(cells));
-
+  console.log("game score = ", scrabbleGame.countScore(player.currentWord, cells));
+  // console.log("horiz position = ", scrabbleGame.checkHorizontalPosition(cells));
+  // console.log("vertical position = ", scrabbleGame.checkVerticalPosition(cells));
+  // console.log("array is a valid word = ",   scrabbleGame.checkValidWord("array"));
   // console.log(word[2]);
 });
 
 
 
-// buildBoardGrid(row, col){
+// buildGameGrid(row, col){
 //     let square = null
-//     let boardArray = []
+//     let GameArray = []
 //     for(var tempRow = 0; tempRow < row; tempRow++){
-//         boardArray.push([])
-//     boardArray[tempRow].push(new Array(col))
+//         GameArray.push([])
+//     GameArray[tempRow].push(new Array(col))
 //     for(var tempCol = 0; tempCol < col; tempCol++){
 //           square = new Square(tempRow, tempCol)
-//           boardArray[tempRow][tempCol] = square.render()
+//           GameArray[tempRow][tempCol] = square.render()
 //         }
 //     }
-//     return boardArray
+//     return GameArray
 //  };
